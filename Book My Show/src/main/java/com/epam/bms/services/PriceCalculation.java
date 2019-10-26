4372c55853a11de0ac86682f9f9ac2d210e12bcd
shivam sharma <shivamsharma.js@gmail.com>
2019-10-26 13:56:54 +0530

    adding tax feature

--- Book My Show/src/main/java/com/epam/bms/services/PriceCalculation.java
+++ Book My Show/src/main/java/com/epam/bms/services/PriceCalculation.java
@@ -1,9 +1,11 @@
 package com.epam.bms.services;
 
 public class PriceCalculation {
-	
-	public double calculatePrice( double cost, int seats)
-	{
-		return cost*seats;
+
+	public double calculatePrice(double cost, int seats) {
+		double totalCost = cost * seats;
+		double tax = (18 * totalCost) / 100;
+		totalCost += tax;
+		return totalCost;
 	}
 }
